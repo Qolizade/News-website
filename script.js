f6023975482f6811941a6d37f843b2bde34212d2
Qolizade <qolizade151@hotmail.com>
2022-12-31 12:47:52 +0330

    set selectors

---
+++ script.js
@@ -0,0 +1,10 @@
+// selectors
+const generalBtn = document.getElementById("general");
+const businessBtn = document.getElementById("business");
+const sportBtn = document.getElementById("sport");
+const technologyBtn = document.getElementById("technology");
+const searchBtn = document.getElementById("search-btn");
+
+const newsQuery = document.getElementById("news-query");
+const newsType = document.getElementById("news-type");
+const newsDetailes = document.getElementById("news-detailes");
